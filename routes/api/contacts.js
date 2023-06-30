e07ff8e8ef2bb54527569c5d9c5b5c1c6572792f
Oleksandra Selezen <113197805+sandra-selezen@users.noreply.github.com>
2023-06-30 15:28:22 +0300

    Updated api

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -42,11 +42,35 @@ router.post('/', async (req, res, next) => {
 })
 
 router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const { error } = addSchema.validate(req.body)
+    if (error) {
+      throw HttpError(400, 'Missing fields')
+    }
+    const { contactId } = req.params
+    const result = await contacts.updateContact(contactId, req.body)
+    if (!result) {
+      throw HttpError(404, 'Not found')
+    }
+    res.json(result)
+  } catch (error) {
+    next(error)
+  }
 })
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const { contactId } = req.params
+    const result = await contacts.removeContact(contactId)
+    if (!result) {
+      throw HttpError(404, 'Not found')
+    }
+    res.json({
+      message: 'Contact deleted'
+    })
+  } catch (error) {
+    next(error)
+  }
 })
 
 module.exports = router
