87838f87bd21bc753c7d905fa0d6844231d071db
Oleksandra Selezen <113197805+sandra-selezen@users.noreply.github.com>
2023-06-30 16:19:31 +0300

    Adds controller

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,75 +2,16 @@ const express = require('express')
 
 const router = express.Router()
 
-const contacts = require('../../models/contacts')
-const { HttpError } = require('../../helpers')
-const { addSchema } = require('../../schemas')
+const ctrl = require('../../controllers/contacts')
 
-router.get('/', async (req, res, next) => {
-  try {
-    const result = await contacts.listContacts()
-    res.json(result)
-  } catch (error) {
-    next(error)
-  }
-})
+router.get('/', ctrl.listContacts)
 
-router.get('/:contactId', async (req, res, next) => {
-  try {
-    const { contactId } = req.params
-    const result = await contacts.getContactById(contactId)
-    if (!result) {
-      throw HttpError(404, 'Not found')
-    }
-    res.json(result)
-  } catch (error) {
-    next(error)
-  }
-})
+router.get('/:contactId', ctrl.getContactById)
 
-router.post('/', async (req, res, next) => {
-  try {
-    const { error } = addSchema.validate(req.body)
-    if (error) {
-      throw HttpError(400, 'Missing fields')
-    }
-    const result = await contacts.addContact(req.body)
-    res.status(201).json(result)
-  } catch (error) {
-    next(error)
-  }
-})
+router.post('/', ctrl.addContact)
 
-router.put('/:contactId', async (req, res, next) => {
-  try {
-    const { error } = addSchema.validate(req.body)
-    if (error) {
-      throw HttpError(400, 'Missing fields')
-    }
-    const { contactId } = req.params
-    const result = await contacts.updateContact(contactId, req.body)
-    if (!result) {
-      throw HttpError(404, 'Not found')
-    }
-    res.json(result)
-  } catch (error) {
-    next(error)
-  }
-})
+router.put('/:contactId', ctrl.updateContact)
 
-router.delete('/:contactId', async (req, res, next) => {
-  try {
-    const { contactId } = req.params
-    const result = await contacts.removeContact(contactId)
-    if (!result) {
-      throw HttpError(404, 'Not found')
-    }
-    res.json({
-      message: 'Contact deleted'
-    })
-  } catch (error) {
-    next(error)
-  }
-})
+router.delete('/:contactId', ctrl.removeContact)
 
 module.exports = router
