21aa700a9cf488d9c1ab4495142060f365b1581e
Oleksandra Selezen <113197805+sandra-selezen@users.noreply.github.com>
2023-07-01 13:56:38 +0300

    Adds contactSchema

---
+++ models/contact.js
@@ -0,0 +1,18 @@
+const { Schema, model } = require('mongoose')
+
+const contactSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, 'Set name for contact'],
+  },
+  email: {
+    type: String,
+  },
+  phone: {
+    type: String,
+  },
+  favorite: {
+    type: Boolean,
+    default: false,
+  },
+})
