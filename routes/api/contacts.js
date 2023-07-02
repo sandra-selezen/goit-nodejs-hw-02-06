b61ba9d98751937bb75c8420d3ee8af1951c2fc5
Oleksandra Selezen <113197805+sandra-selezen@users.noreply.github.com>
2023-07-02 21:50:16 +0300

    Adds authenticate file

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,20 +2,20 @@ const express = require('express')
 
 const { contacts: ctrl } = require('../../controllers')
 const { ctrlWrapper } = require('../../helpers')
-const { isValidId } = require('../../middlewares')
+const { isValidId, authenticate } = require('../../middlewares')
 
 const router = express.Router()
 
-router.get('/', ctrlWrapper(ctrl.listContacts))
+router.get('/', authenticate, ctrlWrapper(ctrl.listContacts))
 
-router.get('/:contactId', isValidId, ctrlWrapper(ctrl.getContactById))
+router.get('/:contactId', authenticate, isValidId, ctrlWrapper(ctrl.getContactById))
 
-router.post('/', ctrlWrapper(ctrl.addContact))
+router.post('/', authenticate, ctrlWrapper(ctrl.addContact))
 
-router.put('/:contactId', isValidId, ctrlWrapper(ctrl.updateContact))
+router.put('/:contactId', authenticate, isValidId, ctrlWrapper(ctrl.updateContact))
 
-router.patch('/:contactId/favorite', isValidId, ctrlWrapper(ctrl.updateStatusContact))
+router.patch('/:contactId/favorite', authenticate, isValidId, ctrlWrapper(ctrl.updateStatusContact))
 
-router.delete('/:contactId', isValidId, ctrlWrapper(ctrl.removeContact))
+router.delete('/:contactId', authenticate, isValidId, ctrlWrapper(ctrl.removeContact))
 
 module.exports = router
