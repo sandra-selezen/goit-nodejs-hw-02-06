edaa22b3d2961516cc1da3025c6a37bcfeed8f6a
Oleksandra Selezen <113197805+sandra-selezen@users.noreply.github.com>
2023-07-03 13:36:24 +0300

    Update listContacts.js

--- controllers/contacts/listContacts.js
+++ controllers/contacts/listContacts.js
@@ -4,8 +4,14 @@ const listContacts = async (req, res) => {
   const { _id: owner } = req.user
   const { page = 1, limit = 20 } = req.query
   const skip = (page - 1) * limit
-  const result = await Contact.find({ owner }, { skip, limit }).populate("owner", "email")
-  res.json(result)
+  const result = await Contact.find({ owner }, "", { skip, limit: Number(limit) }).populate("owner", "email")
+  res.json({
+    status: 'success',
+    code: 200,
+    data: {
+      result,
+    }
+  })
 }
 
 module.exports = listContacts
