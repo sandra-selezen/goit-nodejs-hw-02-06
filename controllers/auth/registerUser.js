2229e6090667499a8575fbb0a95d65946a2e0fd1
Oleksandra Selezen <113197805+sandra-selezen@users.noreply.github.com>
2023-07-03 22:37:29 +0300

    Update registerUser.js

--- controllers/auth/registerUser.js
+++ controllers/auth/registerUser.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt')
+const gravatar = require('gravatar')
 const { User } = require('../../models')
 const { HttpError } = require('../../helpers')
 
@@ -11,12 +12,14 @@ const registerUser = async (req, res) => {
   }
 
   const hashPassword = await bcrypt.hash(password, 10)
+  const avatarURL = gravatar.url(email)
 
   const newUser = await User.create({ ...req.body, password: hashPassword })
 
   res.status(201).json({
     email: newUser.email,
     subscription: newUser.subscription,
+    avatarURL,
   })
 }
 
