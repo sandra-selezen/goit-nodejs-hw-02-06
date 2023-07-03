ef1436769f6c770720af3c1acc577b88c6f92175
Oleksandra Selezen <113197805+sandra-selezen@users.noreply.github.com>
2023-07-03 14:23:02 +0300

    Adds updateSubscriptionSchema

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,13 +2,15 @@ const express = require('express')
 const { users: ctrl } = require('../../controllers')
 const { ctrlWrapper } = require('../../helpers')
 const { validateBody, authenticate } = require('../../middlewares')
-const { registerSchema, loginSchema } = require('../../schemas')
+const { registerSchema, loginSchema, updateSubscriptionSchema } = require('../../schemas')
 
 const router = express.Router()
 
 router.post('/register', validateBody(registerSchema), ctrlWrapper(ctrl.registerUser))
 
 router.post('/login', validateBody(loginSchema), ctrlWrapper(ctrl.loginUser))
+
+router.patch('/subscription', validateBody(updateSubscriptionSchema))
 
 router.get('/current', authenticate, ctrlWrapper(ctrl.getCurrentUser))
 
--- schemas/index.js
+++ schemas/index.js
@@ -1,11 +1,13 @@
 const registerSchema = require('./registerSchema')
 const loginSchema = require('./loginSchema')
-const addSchema = require('./contactSchema');
-const updateFavoriteSchema = require('./updateFavoriteSchema');
+const updateSubscriptionSchema = require('./updateSubscriptionSchema')
+const addSchema = require('./contactSchema')
+const updateFavoriteSchema = require('./updateFavoriteSchema')
 
 module.exports = {
   registerSchema,
   loginSchema,
+  updateSubscriptionSchema,
   addSchema,
   updateFavoriteSchema,
 }
