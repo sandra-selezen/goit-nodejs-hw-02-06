7a7abd1762261084560ed22d276f178dfd810fef
Oleksandra Selezen <113197805+sandra-selezen@users.noreply.github.com>
2023-07-03 12:58:04 +0300

    Update loginSchema.js

--- schemas/loginSchema.js
+++ schemas/loginSchema.js
@@ -3,6 +3,7 @@ const Joi = require('joi')
 const loginSchema = Joi.object({
   password: Joi.string().required(),
   email: Joi.string().required(),
+  token: Joi.string(),
 })
 
 module.exports = loginSchema
