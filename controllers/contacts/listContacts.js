59d0c98dd662427b6490ce092b4e0bd483f51cb0
Oleksandra Selezen <113197805+sandra-selezen@users.noreply.github.com>
2023-07-03 14:10:31 +0300

    Adds favorite filter

--- controllers/contacts/listContacts.js
+++ controllers/contacts/listContacts.js
@@ -2,16 +2,27 @@ const { Contact } = require('../../models')
 
 const listContacts = async (req, res) => {
   const { _id: owner } = req.user
-  const { page = 1, limit = 20 } = req.query
+  const { page = 1, limit = 20, favorite } = req.query
   const skip = (page - 1) * limit
-  const result = await Contact.find({ owner }, "", { skip, limit: Number(limit) }).populate("owner", "email")
-  res.json({
-    status: 'success',
-    code: 200,
-    data: {
-      result,
-    }
-  })
+  if (favorite) {
+    const result = await Contact.find({ owner, favorite }, "", { skip, limit: Number(limit) }).populate("owner", "email")
+    res.json({
+      status: 'success',
+      code: 200,
+      data: {
+        result,
+      }
+    })
+  } else {
+    const result = await Contact.find({ owner }, "", { skip, limit: Number(limit) }).populate("owner", "email")
+    res.json({
+      status: 'success',
+      code: 200,
+      data: {
+        result,
+      }
+    })
+  }
 }
 
 module.exports = listContacts
