90443a00a76b52890aa255eef0b2009a06a1317f
Oleksandra Selezen <113197805+sandra-selezen@users.noreply.github.com>
2023-07-06 20:07:26 +0300

    Send test email

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
 const dotenv = require('dotenv')
+dotenv.config()
+const sgMail = require('@sendgrid/mail')
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 const { authRouter, contactsRouter } = require('./routes/api')
 
-dotenv.config()
+
 
 const app = express()
 
@@ -26,5 +29,21 @@ app.use((err, req, res, next) => {
   const { status = 500, message = 'Server error' } = err
   res.status(status).json({ message, })
 })
+
+// const msg = {
+//   to: 'aleksandra.selezen@gmail.com', // Change to your recipient
+//   from: 'aleksandra.selezen@gmail.com', // Change to your verified sender
+//   subject: 'Sending with SendGrid is Fun',
+//   text: 'and easy to do anywhere, even with Node.js',
+//   html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+// }
+// sgMail
+//   .send(msg)
+//   .then(() => {
+//     console.log('Email sent')
+//   })
+//   .catch((error) => {
+//     console.error(error)
+//   })
 
 module.exports = app
